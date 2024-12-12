a2d3c970aa29214b3e343c6a9ddfa2fa8a17e664
Alenta <martin.fossen@gmail.com>
2024-12-12 15:28:18 +0100

    General clean-up
    Grouped objects in scene, removed unused objects etc.

--- Assets/Floor.cs
+++ Assets/Floor.cs
@@ -4,20 +4,21 @@ using UnityEngine;
 
 public class Floor : MonoBehaviour
 {
+    bool steppedOn = false;
     private SpriteRenderer r;
-    // Start is called before the first frame update
     void Awake()
     {
         r = GetComponentInChildren<SpriteRenderer>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public bool HasBeenWalkedOn()
     {
-        
+        return steppedOn;
     }
+
     public void WalkedOn()
     {
+        steppedOn = true;
         r.enabled = true;
     }
 }
--- Assets/Stuff/Scripts/Guard.cs
+++ Assets/Stuff/Scripts/Guard.cs
@@ -5,33 +5,43 @@ using UnityEngine;
 public class Guard : MonoBehaviour
 {
     public float Speed;
+    public float CheckTime;
     Rigidbody rb;
-    [SerializeField]
     bool move;
-    [SerializeField]
     bool turn;
-    private void Awake() {
+    bool inMaze = true;
+    int steps = 0;
+    private void Awake()
+    {
         rb = GetComponent<Rigidbody>();
     }
 
     private void FixedUpdate()
     {
-        if (move) {
+        if (move)
+        {
             rb.velocity = transform.forward * Speed * Time.deltaTime;
         }
     }
 
-
     private void OnCollisionEnter(Collision collision)
     {
-        if(collision.gameObject.CompareTag("Wall") && !turn)
+        if (collision.gameObject.CompareTag("Wall") && !turn)
         {
             StartCoroutine(Turn());
         }
-        if (collision.gameObject.CompareTag("Floor")) { collision.collider.gameObject.GetComponent<Floor>().WalkedOn(); }
+        if (collision.gameObject.CompareTag("Floor")) { Floor floor = collision.gameObject.GetComponentInChildren<Floor>();
+            if (floor && !floor.HasBeenWalkedOn())
+            {
+                steps++;
+                floor.WalkedOn();
+            }
+            else print(floor);
+        }
     }
     public void Move()
     {
+        StartCoroutine(BoundaryCheck());
         move = true;
     }
 
@@ -47,5 +57,25 @@ public class Guard : MonoBehaviour
         Move();
         yield return new WaitForSeconds(0.1f);
         turn = false;
+    }
+
+    IEnumerator BoundaryCheck()
+    {
+        while (inMaze)
+        {
+            inMaze = IsWithinTheMaze();
+            yield return new WaitForSeconds(CheckTime);         
+        }
+        print("I left the maze at " + (steps-1) + " unique steps");
+    }
+
+    bool IsWithinTheMaze()
+    {
+        if (transform.position.x < 0 || transform.position.x > 128 ||
+            transform.position.z < 0 || transform.position.z > 135)
+        {
+            return false;
+        }
+        else return true;
     }
 }
